0e1be591056d83085db6c4613f34986aa3361bea
rlManzo <ricardojuliomanzo@gmail.com>
2023-05-31 16:41:20 -0300

    primer maquetado de seccion productos

--- scripts/main.js
+++ scripts/main.js
@@ -1,20 +1,25 @@
 class Producto{
-    constructor(id,nombre, img,categoria){
+    constructor(id,nombre, img, info,categoria){
         this.id = id;
         this.nombre = nombre;
         this.img = img;
+        this.info = info;
         this.categoria = categoria;
         this.cantidad = 1;
       }
     }
 
-const producto1 = new Producto(1,"remera1","","vinilo");
-const producto2 = new Producto(2,"remera2","","sublimable");
-const producto3 = new Producto(3,"remera3","","polimero");
-const producto4 = new Producto(4,"remera4","","3d");
-const producto5 = new Producto(5,"remera5","","sublimable");
+const producto1 = new Producto(1,"remera sublimada","https://d2r9epyceweg5n.cloudfront.net/stores/842/939/products/remera-arg-firmas-blanca-011-05cf22ab7fc24c586f16764145168407-1024-1024.jpg","vinilo");
+const producto2 = new Producto(2,"remera vinilo","https://d2r9epyceweg5n.cloudfront.net/stores/842/939/products/remera-arg-firmas-blanca-011-05cf22ab7fc24c586f16764145168407-1024-1024.jpg","sublimable");
+const producto3 = new Producto(3,"remera serigrafia","https://d2r9epyceweg5n.cloudfront.net/stores/842/939/products/remera-arg-firmas-blanca-011-05cf22ab7fc24c586f16764145168407-1024-1024.jpg","polimero");
+const producto4 = new Producto(4,"remera sublimada","https://d2r9epyceweg5n.cloudfront.net/stores/842/939/products/remera-arg-firmas-blanca-011-05cf22ab7fc24c586f16764145168407-1024-1024.jpg","3d");
+const producto5 = new Producto(5,"remera sublimada","https://d2r9epyceweg5n.cloudfront.net/stores/842/939/products/remera-arg-firmas-blanca-011-05cf22ab7fc24c586f16764145168407-1024-1024.jpg","sublimable");
+const producto6 = new Producto(6,"remera serigrafia","https://d2r9epyceweg5n.cloudfront.net/stores/842/939/products/remera-arg-firmas-blanca-011-05cf22ab7fc24c586f16764145168407-1024-1024.jpg","polimero");
+const producto7 = new Producto(7,"remera sublimada","https://d2r9epyceweg5n.cloudfront.net/stores/842/939/products/remera-arg-firmas-blanca-011-05cf22ab7fc24c586f16764145168407-1024-1024.jpg","3d");
+const producto8 = new Producto(8,"remera sublimada","https://d2r9epyceweg5n.cloudfront.net/stores/842/939/products/remera-arg-firmas-blanca-011-05cf22ab7fc24c586f16764145168407-1024-1024.jpg","sublimable");
 
-const arrayProductos = [producto1, producto2, producto3, producto4, producto5];
+
+const arrayProductos = [producto1, producto2, producto3, producto4, producto5, producto6,producto7,producto8];
 
 const carrito = [];
 
@@ -24,15 +29,15 @@ const contenedorProductos = document.getElementById("contenedorProductos");
 function mostrarProductos(){
   arrayProductos.forEach(producto =>{
     const card = document.createElement("div");
-    card.classList.add( "col-xl-3", "col-md-6", "col-xs-12");
+    card.classList.add( "col-xl-3", "col-6", "col-xs-12");
     card.innerHTML =  `
-                      <div class="">
-                         <img src="${producto.img}" class="" alt="${producto.nombre}">
+                      <div class="cardStyles position-relative text-center col-6">
+                         <img class="cardImg" src="${producto.img}" class="" alt="${producto.nombre}">
                            <div>
                             <h5 class="card-title text-center">${producto.nombre}</h5>
-                            <h5 class="card-title text-center">${producto.informacion}</h5>
-                            <button class="btn btn-primary botonHeader" id= boton${producto.id}>Agregar</button>
-                            <button class="btn btn-primary" id= boton>Pedir info</button>
+                            <h5 class="card-title text-center h6">${producto.info}</h5>
+                            <button class=" botonHeader botonProductos1" id= boton${producto.id}>Agregar</button>
+                            <button class="botonHeader botonProductos2" id= boton><a class="" href="https://api.whatsapp.com/send?phone=1131686767&text=Hola!%20Me%20gustaria%20saber%20mas%20%20sobre%20el%20siguiente%20articulo:%0A%0A${producto.nombre}%20Categoria:%0A%0A${producto.info}%0A%0A" target="_blank"</> info</button>
                            </div>
                       </div>`
     
@@ -79,16 +84,21 @@ function mostrarCarrito(){
         innerHTML = `${producto.nombre} ${producto.precio}`
         card.innerHTML =  `
                           <div class="container">
-                          <div class="img1">
-                          <img src="${producto.img}" class="imgProductos" alt="${producto.nombre}">
-                           <button class="botonEliminar" id= eliminar${producto.id}>
+                          <div >
+                          <button class="botonEliminar" id= eliminar${producto.id}>
                           <i><svg width="20" height="19" viewBox="0 0 20 19" fill="none" xmlns="http://www.w3.org/2000/svg">
                           <path fill-rule="evenodd" clip-rule="evenodd" d="M7.75598 0.955026H1.10607C0.698197 0.955026 0.367188 1.28268 0.367188 1.68641C0.367188 2.09015 0.698197 2.4178 1.10607 2.4178H1.89675L2.94752 16.491C3.03328 17.6334 3.9953 18.5185 5.15377 18.5185H14.7917C15.9503 18.5185 16.9124 17.6334 16.998 16.491L18.0487 2.4178H18.8394C19.2473 2.4178 19.5783 2.09015 19.5783 1.68641C19.5783 1.28268 19.2473 0.955026 18.8394 0.955026H12.1895V0.953556C12.1895 0.549822 11.8585 0.222168 11.4506 0.222168H8.49511C8.08724 0.222168 7.75623 0.549822 7.75623 0.953556V0.955026H7.75598ZM16.5664 2.4178H3.37886L4.42072 16.3827C4.45024 16.7616 4.76954 17.0557 5.15365 17.0557H14.7916C15.1759 17.0557 15.495 16.7617 15.5245 16.3827L16.5664 2.4178ZM7.0171 6.08503V11.9361C7.0171 12.3399 7.34811 12.6675 7.75598 12.6675C8.16385 12.6675 8.49486 12.3399 8.49486 11.9361V6.08503C8.49486 5.68129 8.16385 5.35364 7.75598 5.35364C7.34811 5.35364 7.0171 5.68129 7.0171 6.08503ZM11.4504 6.08503V11.9361C11.4504 12.3399 11.7814 12.6675 12.1893 12.6675C12.5971 12.6675 12.9281 12.3399 12.9281 11.9361V6.08503C12.9281 5.68129 12.5971 5.35364 12.1893 5.35364C11.7814 5.35364 11.4504 5.68129 11.4504 6.08503Z" fill="#EA5534"/>
                           </svg>
                           </i></button>
-                          <h5 class="card-title ">${producto.nombre}</h5>
-                          <h6 class="card-title ">${producto.categoria}</h>
-                          <div>
+                          <img src="${producto.img}" class="img1" alt="${producto.nombre}">
+                          
+                          <div class="titleCards">
+                          <h5 class="h6">${producto.nombre}</h5>
+                          <h6 class="h8">${producto.categoria}</h>
+                          </div>
+                          
+                          
+                          
                          
           </div>
        `
@@ -135,7 +145,7 @@ const sendcarrito = () => {
   
 }
 
-//funcion Buscador 
+//funcion Buscador <h5 class="textoBuscador" >${producto.nombre}</h5>
 const buscador = document.getElementById("buscador");
 const resultado = document.getElementById("resultado");
 const busqueda = document.getElementById("busqueda");
@@ -149,11 +159,12 @@ const filtrar = () => {
                 if ( nombre.indexOf(texto) !== -1){
                     resultado.innerHTML += 
                     
-                    `<li class="listStyle">
-                        <img src=${producto.img} <h5 class="textoBuscador" >${producto.nombre}</h5>
-                        <p></p></li>
-                        <button class="botonHeader" id= botton${producto.id}>ver producto</button>`    
-                    
+                    `<li class="listStyles">
+                       <img class="img1"  src=${producto.img} />
+                        
+                      
+                        <button class="botonProductos1 botonHeader botonBuscador" id= botton${producto.id}>ver producto</button>    
+                    </li>`
                     busqueda.style.opacity = 1;
                   
                     const botton = document.getElementById(`botton${producto.id}`);
