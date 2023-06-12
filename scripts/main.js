615af4a788566f49933c36d71bb5aab576c1862c
rlManzo <ricardojuliomanzo@gmail.com>
2023-06-12 16:51:32 -0300

    responsive de seccion productos

--- scripts/main.js
+++ scripts/main.js
@@ -50,7 +50,7 @@ const cardSeccion = (producto)=>{
      </div>
      <div class="infoCard">
      <button class=" botonHeader botonProductos1" id= boton${producto.id} onClick="agregarAlCarrito(${producto.id},agregarNumeroCarrito())">AGREGAR</button>
-     <button class="botonHeader botonProductos2" id= boton><a class="" href="https://api.whatsapp.com/send?phone=1131686767&text=Hola!%20Me%20gustaria%20saber%20mas%20%20sobre%20el%20siguiente%20articulo:%0A%0A${producto.nombre}%20Categoria:%0A%0A${producto.info}%0A%0A" target="_blank"</>INFO</button>
+     <button class="botonHeader botonProductos2" id= boton><a class="botonInfo" href="https://api.whatsapp.com/send?phone=1131686767&text=Hola!%20Me%20gustaria%20saber%20mas%20%20sobre%20el%20siguiente%20articulo:%0A%0A${producto.nombre}%20Categoria:%0A%0A${producto.info}%0A%0A" target="_blank"</><i><img class="iconoWhatapps" src="./assets/iconoWhatapps.svg" alt=""></i> Info</button>
      
      </div>
 </div>`
@@ -226,14 +226,14 @@ function agregarNumeroCarrito(){
 
 //funcion productos en el carrito
 function mostrarCarrito(){
-      contenedorCarrito.innerHTML = "";
+      contenedorCarrito.innerHTML = "Sin productos en carrito";
       carrito.forEach(producto => {
         const card = document.createElement("div");
         card.classList.add( "col-xl-3", "col-md-6", "col-xs-12");
         innerHTML = `${producto.nombre} ${producto.precio}`
-        card.innerHTML =  `
-                          <div class="container">
-                          <div >
+        contenedorCarrito.innerHTML =  `
+                          <div class="container ">
+                          <div class="">
                           <button class="botonEliminar" id= eliminar${producto.id}>
                           <i><svg width="20" height="19" viewBox="0 0 20 19" fill="none" xmlns="http://www.w3.org/2000/svg">
                           <path fill-rule="evenodd" clip-rule="evenodd" d="M7.75598 0.955026H1.10607C0.698197 0.955026 0.367188 1.28268 0.367188 1.68641C0.367188 2.09015 0.698197 2.4178 1.10607 2.4178H1.89675L2.94752 16.491C3.03328 17.6334 3.9953 18.5185 5.15377 18.5185H14.7917C15.9503 18.5185 16.9124 17.6334 16.998 16.491L18.0487 2.4178H18.8394C19.2473 2.4178 19.5783 2.09015 19.5783 1.68641C19.5783 1.28268 19.2473 0.955026 18.8394 0.955026H12.1895V0.953556C12.1895 0.549822 11.8585 0.222168 11.4506 0.222168H8.49511C8.08724 0.222168 7.75623 0.549822 7.75623 0.953556V0.955026H7.75598ZM16.5664 2.4178H3.37886L4.42072 16.3827C4.45024 16.7616 4.76954 17.0557 5.15365 17.0557H14.7916C15.1759 17.0557 15.495 16.7617 15.5245 16.3827L16.5664 2.4178ZM7.0171 6.08503V11.9361C7.0171 12.3399 7.34811 12.6675 7.75598 12.6675C8.16385 12.6675 8.49486 12.3399 8.49486 11.9361V6.08503C8.49486 5.68129 8.16385 5.35364 7.75598 5.35364C7.34811 5.35364 7.0171 5.68129 7.0171 6.08503ZM11.4504 6.08503V11.9361C11.4504 12.3399 11.7814 12.6675 12.1893 12.6675C12.5971 12.6675 12.9281 12.3399 12.9281 11.9361V6.08503C12.9281 5.68129 12.5971 5.35364 12.1893 5.35364C11.7814 5.35364 11.4504 5.68129 11.4504 6.08503Z" fill="#EA5534"/>
@@ -287,7 +287,7 @@ const sendcarrito = () => {
   
   
   
-  mandarCarrito.innerHTML = ` <button class="text-center botonHeader"" ><a class="" href="https://api.whatsapp.com/send?phone=1131686767&text=Hola!%20Me%20gustaria%20hacer%20el%20siguiente%20pedido:%0A%0A${totalCompra}%0A%0A" target="_blank"</>Pedir Presupuesto</button>`
+  mandarCarrito.innerHTML = ` <button class="text-center botonHeader"" ><a class="botonPresupuesto" href="https://api.whatsapp.com/send?phone=1131686767&text=Hola!%20Me%20gustaria%20hacer%20el%20siguiente%20pedido:%0A%0A${totalCompra}%0A%0A" target="_blank"</>Pedir Presupuesto</button>`
   
    
   
@@ -311,6 +311,9 @@ const filtrar = () => {
                     
                     `<li class="listStyles">
                        <img class="img1"  src=${producto.img} />
+                       <div>
+                       <h6 class="textBuscador">${producto.nombre}</h5>
+                       </div>
                        <button class="botonProductos2 botonHeader botonBuscador" id= botton${producto.id} onClick="agregarAlModal(${producto.id}),mostrarDetalleProducto()" data-bs-toggle="modal" data-bs-target="#staticBackdrop" ><img class="ojoStyle" src="./assets/ojo.png"/></button>    
                     </li>`
                     busqueda.style.opacity = 1;
