fee784cffc98b7a083091df0e7caa75c59d41494
rlManzo <ricardojuliomanzo@gmail.com>
2023-06-19 15:39:59 -0300

    cambios css agregados a seccion productos

--- scripts/main.js
+++ scripts/main.js
@@ -9,15 +9,15 @@ class Producto{
       }
     }
 
-const producto1 = new Producto(1,"remera sublimada","https://d2r9epyceweg5n.cloudfront.net/stores/842/939/products/remera-arg-firmas-blanca-011-05cf22ab7fc24c586f16764145168407-1024-1024.jpg","vinilo","vinilo");
-const producto2 = new Producto(2,"remera vinilo","https://d2r9epyceweg5n.cloudfront.net/stores/842/939/products/remera-arg-firmas-blanca-011-05cf22ab7fc24c586f16764145168407-1024-1024.jpg","sublimable","sublimable");
-const producto3 = new Producto(3,"remera serigrafia","https://d2r9epyceweg5n.cloudfront.net/stores/842/939/products/remera-arg-firmas-blanca-011-05cf22ab7fc24c586f16764145168407-1024-1024.jpg","polimero","vinilo");
-const producto4 = new Producto(4,"remera sublimada","https://d2r9epyceweg5n.cloudfront.net/stores/842/939/products/remera-arg-firmas-blanca-011-05cf22ab7fc24c586f16764145168407-1024-1024.jpg","3d","sublimable");
-const producto5 = new Producto(5,"remera sublimada","https://d2r9epyceweg5n.cloudfront.net/stores/842/939/products/remera-arg-firmas-blanca-011-05cf22ab7fc24c586f16764145168407-1024-1024.jpg","sublimable","sublimable");
-const producto6 = new Producto(6,"remera serigrafia","https://d2r9epyceweg5n.cloudfront.net/stores/842/939/products/remera-arg-firmas-blanca-011-05cf22ab7fc24c586f16764145168407-1024-1024.jpg","polimero","vinilo");
-const producto7 = new Producto(7,"remera sublimada","https://d2r9epyceweg5n.cloudfront.net/stores/842/939/products/remera-arg-firmas-blanca-011-05cf22ab7fc24c586f16764145168407-1024-1024.jpg","3d","vinilo");
-const producto8 = new Producto(8,"remera sublimada","https://d2r9epyceweg5n.cloudfront.net/stores/842/939/products/remera-arg-firmas-blanca-011-05cf22ab7fc24c586f16764145168407-1024-1024.jpg","sublimable","dtf");
-const producto9 = new Producto(9,"remera sublimada","https://d2r9epyceweg5n.cloudfront.net/stores/842/939/products/remera-arg-firmas-blanca-011-05cf22ab7fc24c586f16764145168407-1024-1024.jpg","sublimable","dtf");
+const producto1 = new Producto(1,"Remera sublimada","https://http2.mlstatic.com/D_NQ_NP_815250-MLA49035064606_022022-O.webp","vinilo","vinilo");
+const producto2 = new Producto(2,"Remera vinilo","https://http2.mlstatic.com/D_NQ_NP_815250-MLA49035064606_022022-O.webp","sublimable","sublimable");
+const producto3 = new Producto(3,"Remera serigrafia","https://http2.mlstatic.com/D_NQ_NP_815250-MLA49035064606_022022-O.webp","polimero","vinilo");
+const producto4 = new Producto(4,"Remera sublimada","https://http2.mlstatic.com/D_NQ_NP_815250-MLA49035064606_022022-O.webp","3d","sublimable");
+const producto5 = new Producto(5,"Remera sublimada","https://http2.mlstatic.com/D_NQ_NP_815250-MLA49035064606_022022-O.webp","sublimable","sublimable");
+const producto6 = new Producto(6,"Remera serigrafia","https://http2.mlstatic.com/D_NQ_NP_815250-MLA49035064606_022022-O.webp","polimero","vinilo");
+const producto7 = new Producto(7,"Remera sublimada","https://http2.mlstatic.com/D_NQ_NP_815250-MLA49035064606_022022-O.webp","3d","vinilo");
+const producto8 = new Producto(8,"Remera sublimada","https://http2.mlstatic.com/D_NQ_NP_815250-MLA49035064606_022022-O.webp","sublimable","dtf");
+const producto9 = new Producto(9,"Remera sublimada","https://http2.mlstatic.com/D_NQ_NP_815250-MLA49035064606_022022-O.webp","sublimable","dtf");
 
 const arrayProductos = [producto1, producto2, producto3, producto4, producto5, producto6,producto7,producto8,producto9];
 console.log(arrayProductos)
@@ -50,7 +50,7 @@ const cardSeccion = (producto)=>{
      </div>
      <div class="infoCard">
      <button class=" botonHeader botonProductos1" id= boton${producto.id} onClick="agregarAlCarrito(${producto.id},agregarNumeroCarrito())">AGREGAR</button>
-     <button class="botonHeader botonProductos2" id= boton><a class="botonInfo" href="https://api.whatsapp.com/send?phone=1131686767&text=Hola!%20Me%20gustaria%20saber%20mas%20%20sobre%20el%20siguiente%20articulo:%0A%0A${producto.nombre}%20Categoria:%0A%0A${producto.info}%0A%0A" target="_blank"</><i><img class="iconoWhatapps" src="./assets/iconoWhatapps.svg" alt=""></i> Info</button>
+     <button class="botonHeader botonProductos2" id= boton><a class="botonInfo" href="https://api.whatsapp.com/send?phone=1131686767&text=Hola!%20Me%20gustaria%20saber%20mas%20%20sobre%20el%20siguiente%20articulo:%0A%0A${producto.nombre}%20Categoria:%0A%0A${producto.info}%0A%0A" target="_blank"</><i><img class="iconoWhatapps" src="./assets/whatappsLogo.svg" alt=""></i>Pedir Info</button>
      
      </div>
 </div>`
@@ -97,6 +97,7 @@ cardSeccion(producto)
 cardSeccion(producto)
   });
  }) 
+
 
 //funcion agregar al carrito desde el boton de las cards
 
@@ -148,9 +149,10 @@ function mostrarDetalleProducto(){
   contenedorDetalleProducto.innerHTML = "";
     modal.forEach(producto => {
     const card = document.createElement("div"); 
+    
     innerHTML = `${producto.nombre} ${producto.precio}`
     card.innerHTML =  `
-                      <div class="row g-0 ">
+                      <div class="row g-0 data-aos="fade-right"">
                         <div class="col-md-2">
                         <img src="${producto.img}" class="imgModalDetail card-img-top" alt="${producto.nombre}">
                         <img src="${producto.img}" class="imgModalDetail card-img-top" alt="${producto.nombre}">
@@ -226,15 +228,15 @@ function agregarNumeroCarrito(){
 
 //funcion productos en el carrito
 function mostrarCarrito(){
-      contenedorCarrito.innerHTML = "Sin productos en carrito";
+      contenedorCarrito.innerHTML = "";
       carrito.forEach(producto => {
         const card = document.createElement("div");
         card.classList.add( "col-xl-3", "col-md-6", "col-xs-12");
         innerHTML = `${producto.nombre} ${producto.precio}`
-        contenedorCarrito.innerHTML =  `
-                          <div class="container ">
+        contenedorCarrito.innerHTML +=  `
+                          <div class="container carritoContainer">
                           <div class="">
-                          <button class="botonEliminar" id= eliminar${producto.id}>
+                          <button class="botonEliminar" id= eliminar${producto.id} onClick="eliminarProducto(${producto.id})">
                           <i><svg width="20" height="19" viewBox="0 0 20 19" fill="none" xmlns="http://www.w3.org/2000/svg">
                           <path fill-rule="evenodd" clip-rule="evenodd" d="M7.75598 0.955026H1.10607C0.698197 0.955026 0.367188 1.28268 0.367188 1.68641C0.367188 2.09015 0.698197 2.4178 1.10607 2.4178H1.89675L2.94752 16.491C3.03328 17.6334 3.9953 18.5185 5.15377 18.5185H14.7917C15.9503 18.5185 16.9124 17.6334 16.998 16.491L18.0487 2.4178H18.8394C19.2473 2.4178 19.5783 2.09015 19.5783 1.68641C19.5783 1.28268 19.2473 0.955026 18.8394 0.955026H12.1895V0.953556C12.1895 0.549822 11.8585 0.222168 11.4506 0.222168H8.49511C8.08724 0.222168 7.75623 0.549822 7.75623 0.953556V0.955026H7.75598ZM16.5664 2.4178H3.37886L4.42072 16.3827C4.45024 16.7616 4.76954 17.0557 5.15365 17.0557H14.7916C15.1759 17.0557 15.495 16.7617 15.5245 16.3827L16.5664 2.4178ZM7.0171 6.08503V11.9361C7.0171 12.3399 7.34811 12.6675 7.75598 12.6675C8.16385 12.6675 8.49486 12.3399 8.49486 11.9361V6.08503C8.49486 5.68129 8.16385 5.35364 7.75598 5.35364C7.34811 5.35364 7.0171 5.68129 7.0171 6.08503ZM11.4504 6.08503V11.9361C11.4504 12.3399 11.7814 12.6675 12.1893 12.6675C12.5971 12.6675 12.9281 12.3399 12.9281 11.9361V6.08503C12.9281 5.68129 12.5971 5.35364 12.1893 5.35364C11.7814 5.35364 11.4504 5.68129 11.4504 6.08503Z" fill="#EA5534"/>
                           </svg>
@@ -250,13 +252,13 @@ function mostrarCarrito(){
        `
 
 contenedorCarrito.appendChild(card);
-      
+ /*     
 const botonEliminarProducto = document.getElementById(`eliminar${producto.id}`)
 botonEliminarProducto.addEventListener("click", ()=>{
   eliminarProducto(producto.id);  
   contador--;
-  numeroCarrito.textContent = contador
-   });
+  
+   });*/
    });
 }
  
@@ -265,9 +267,10 @@ botonEliminarProducto.addEventListener("click", ()=>{
 function eliminarProducto(id){
   const producto = carrito.find(Producto => Producto.id === id);
   const indice = carrito.indexOf(producto);
-  carrito.splice(indice, 1);
+  carrito.pop();
+  contador = 0; 
   mostrarCarrito();
-  
+  numeroCarrito.textContent = contador
 }
 
 //funcion que envia al whatapps la lista del carrito
